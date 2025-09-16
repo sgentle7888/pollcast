bd53a2de3ed0cc290b18869c8e25e3111f5491e8
Sgentle <sgentled@gmail.com>
2025-09-16 16:22:35 +0100

    poll and sruvey migration fixed

--- pollcast/pollcast/doctype/poll_response/poll_response.py
+++ pollcast/pollcast/doctype/poll_response/poll_response.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2025, Godwin Ariwodo and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.utils import now_datetime
 
 class PollResponse(Document):
-	pass
+    def before_insert(self):
+        if not self.creation_timestamp:
+            self.creation_timestamp = now_datetime()
+
